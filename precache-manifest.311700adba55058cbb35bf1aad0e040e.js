4a4811046dcd2ae788d3b4f630fc6b7d7fbfa5e9
JohnGaltRu <ntts2@list.ru>
2019-06-13 21:41:30 +0300

    Updates

---
+++ precache-manifest.311700adba55058cbb35bf1aad0e040e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97434ef36c354c0a2b416d78c59472a",
+    "url": "/currency-converter-app/index.html"
+  },
+  {
+    "revision": "b5f3160dcfff70dc8488",
+    "url": "/currency-converter-app/static/css/main.b8bf4246.chunk.css"
+  },
+  {
+    "revision": "5e029b08071a6f12dce3",
+    "url": "/currency-converter-app/static/js/2.2b0a35d4.chunk.js"
+  },
+  {
+    "revision": "b5f3160dcfff70dc8488",
+    "url": "/currency-converter-app/static/js/main.c86d5ad1.chunk.js"
+  },
+  {
+    "revision": "718bf1a361b9f3c248d0",
+    "url": "/currency-converter-app/static/js/runtime~main.de6384ee.js"
+  },
+  {
+    "revision": "8ba3391eb9f58ef83b959400b15a409b",
+    "url": "/currency-converter-app/static/media/IMDB.8ba3391e.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/currency-converter-app/static/media/React.9a28da9f.svg"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/currency-converter-app/static/media/TheMovieDB.02a9430b.svg"
+  }
+]);
